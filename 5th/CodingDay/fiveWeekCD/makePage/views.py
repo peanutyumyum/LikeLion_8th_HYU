58a4d6e74a243abebe8c1a8252290fd0ea431202
peanutyumyum <jrimit@gmail.com>
2020-05-23 03:12:10 +0900

    5weekCodingDayWorkModify.views

--- 5th/CodingDay/fiveWeekCD/makePage/views.py
+++ 5th/CodingDay/fiveWeekCD/makePage/views.py
@@ -7,4 +7,4 @@ def home(request):
     youTube1 = youTubeChannel.objects.get(id=1)
     youTube2 = youTubeChannel.objects.get(id=2)
     youTube3 = youTubeChannel.objects.get(id=3)
-    return render(request, 'home.html',{'youtube1':youTube1, 'youtube2':youTube2, 'youTube3':youTube3})
+    return render(request, 'home.html',{'youtube1':youTube1, 'youtube2':youTube2, 'youtube3':youTube3})
